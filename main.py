813541c81132815a0ef87bda1e29e8eb22ddae6f
nickr77 <nickroberts57@gmail.com>
2016-03-02 15:37:35 -0600

    Started writing web crawler

--- main.py
+++ main.py
@@ -2,11 +2,32 @@ __author__ = 'Nick Roberts -- Steven Bock'
 
 import re
 import mechanize
+from BeautifulSoup import BeautifulSoup
+#Needs to return: list of pages, list of outgoing links, amount of JPG files, save words from .txt .htm .html files, stem words before storing in list/dictionary
 
 browse = mechanize.Browser()
-page = browse.open("http://lyle.smu.edu/~fmoore")
+#CONFIG SECTION
+browse.set_handle_robots(True)
 
-print page.read()
-print browse.geturl()
-for links in browse.links():
-    print links
+#END CONFIG SECTION
+#GLOBAL VARS
+baseUrl = "http://lyle.smu.edu/~fmoore"
+urlList = [baseUrl]
+visitedUrls = [baseUrl]
+retrieveLimit = 0
+stopWords = []
+links = []
+outgoingLinks = []
+jpgAmount = 0
+words = {}
+
+#END GLOBAL VARS
+
+browse.open(baseUrl)
+soup = BeautifulSoup(browse.response().read())
+wordsInPage = soup.getText().split()
+for x in wordsInPage:
+    print x
+
+
+
