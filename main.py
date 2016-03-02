f4aa99f16c2d828c9dfac27382e06a2d50bdbe58
nickr77 <nickroberts57@gmail.com>
2016-03-02 16:01:39 -0600

    Started writing web crawler

--- main.py
+++ main.py
@@ -18,16 +18,36 @@ retrieveLimit = 0
 stopWords = []
 links = []
 outgoingLinks = []
+badLinks = []
 jpgAmount = 0
 words = {}
 
 #END GLOBAL VARS
+stopFile = open("stopwords.txt", "r")
+lines = stopFile.read()
+stopWords = lines.split()
 
-browse.open(baseUrl)
-soup = BeautifulSoup(browse.response().read())
-wordsInPage = soup.getText().split()
-for x in wordsInPage:
-    print x
+for page in urlList:
+
+    visitedUrls.append(page)
+
+
+    browse.open(page)
+    soup = BeautifulSoup(browse.response().read())
+    wordsInPage = soup.getText().split()
+
+    #get all links, check if in list already or visited
+    #make sure urls are actually different
+    #count jpgs
+    #if link doesn't work, add to badLinks list
+
+
+
+
+
+    for x in wordsInPage: #parse and stem words, add to dictionary
+        if x not in stopWords:
+            print x
 
 
 
