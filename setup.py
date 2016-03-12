c4366ab9c381d8efa859cae82c4d4ad3f46a4aec
Nick Roberts <dev.nickroberts@gmail.com>
2016-03-11 17:10:57 -0800

    Added installer for dependencies.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='webcrawler',
+    version='0.1',
+    py_modules=['main'],
+    url='https://github.com/nickr77/project1webcrawler',
+    install_requires=['BeautifulSoup', 'Mechanize'],
+    author='nickr',
+    author_email='robertsn@smu.edu',
+    description='Web Crawler Project for CSE 5337; Steven Bock - Nick Roberts'
+)
