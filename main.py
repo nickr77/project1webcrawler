a992f562f2790cf37913bc4feb4677fd29ae5b98
nickr77 <nickroberts57@gmail.com>
2016-03-13 17:10:57 -0500

    Stemming words now. DocID's, frequent word list added. Seems to be doing everything required. Just need to write results to a file

--- main.py
+++ main.py
@@ -21,12 +21,15 @@ robots.read()
 urlList = [baseUrl]
 visitedUrls = [baseUrl]
 retrieveLimit = 0
-stopWords = []
+stopWords = ()
 links = []
 outgoingLinks = []
 badLinks = []
 jpgAmount = 0
 words = {}
+docIDCounter = 1
+documentIDs = []
+stemmer = snowballstemmer.stemmer("english")
 #END GLOBAL VARS
 
 print robots.path
@@ -46,7 +49,7 @@ for page in urlList:
 
     try:
         browse.open(page)
-        soup = BeautifulSoup(browse.response().read())
+        soup = BeautifulSoup(browse.response().read()) #if can't parse, assumed to be binary file or 404
         wordsInPage = soup.getText().split()
         for link in browse.links():
             tempURL = urlparse.urljoin(link.base_url, link.url)
@@ -67,9 +70,21 @@ for page in urlList:
                 else:
                     outgoingLinks.append(tempURL)
 
+        documentIDs.append((docIDCounter, page))
+
         for x in wordsInPage: #parse and stem words, add to dictionary
-            if x not in stopWords:
+            x = x.replace(",", "") #removes commas before checking for stopwords
+            if x not in stopWords and not set('[~=!@-<>#$%^&*()_+{}":;\']+$').intersection(x): #gets rid of invalid words
                 temp = x
+                temp = temp.lower()
+                temp = stemmer.stemWord(temp)
+                #print temp
+                if temp not in words.keys():
+                    words[temp] = [docIDCounter]
+                else:
+                    words[temp].append(docIDCounter)
+        docIDCounter += 1 #increments doc ID after successful parsing
+
 
                 #index words here
     except: #occurs if it is a binary file or non-existent file
@@ -79,8 +94,16 @@ for page in urlList:
         if browse.response().code == 404:
             badLinks.append(page)
 
+wordFreqency = []
+for x in words.keys():
+    wordFreqency.append((str(x), len(words[x])))
+    print len(words[x])
+frequentWords = sorted(wordFreqency, key=lambda x: x[1])
+frequentWords.reverse()
 
+print "Most Frequent Words: ", frequentWords[:20]
 print "OUTGOING LINKS: ",outgoingLinks #for debugging, remove once complete
 print "Good URLS: ",urlList
 print "BAD LINKS: ",badLinks
 print "JPEGS: ", jpgAmount
+print "DOCIDS : ", documentIDs
