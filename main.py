6ae032b3927145ee13e0b8bff1c12f30cc7e7b3a
nickr77 <nickroberts57@gmail.com>
2016-03-14 15:07:16 -0500

    Added output files, fixed issue where urls ending in '/' would be identified differently

--- main.py
+++ main.py
@@ -31,22 +31,17 @@ docIDCounter = 1
 documentIDs = []
 stemmer = snowballstemmer.stemmer("english")
 #END GLOBAL VARS
-
-print robots.path
-print robots.can_fetch("*", "/dontgohere/")
-print robots.can_fetch("*", "/dontgohere/index.html")
-
-
 temp = input("Enter page limit: ")
 retrieveLimit = int(temp)
 stopFile = open("stopwords.txt", "r")
 lines = stopFile.read()
 stopWords = lines.split()
 
+print "Crawling Pages, please wait..."
 for page in urlList:
     if docIDCounter > retrieveLimit:
         break
-    visitedUrls.append(page)
+    #visitedUrls.append(page)
 
     try:
         browse.open(page)
@@ -64,12 +59,11 @@ for page in urlList:
             if tempURL not in urlList:
                 if tempURL.startswith(baseUrl):
                     if robots.can_fetch("*", "/" + link.url):
-                        print "WE CAN FETCH IT ", tempURL
+                        #print "WE CAN FETCH IT ", tempURL
                         urlList.append(tempURL)
-                    else:
-                        print "We can't fetch it: ", tempURL
                 else:
-                    outgoingLinks.append(tempURL)
+                    if tempURL + "/" not in urlList:
+                        outgoingLinks.append(tempURL)
 
         documentIDs.append((docIDCounter, page))
 
@@ -90,12 +84,12 @@ for page in urlList:
 
                 #index words here
     except: #occurs if it is a binary file or non-existent file
-        print "Couldn't open that link", page
+        #print "Couldn't open that link", page
         if page.endswith(".jpg"):
             jpgAmount += 1
         if browse.response().code == 404:
             badLinks.append(page)
-
+print "Computing Word frequency"
 wordFreqency = []
 for x in words.keys():
     wordFreqency.append((str(x), len(words[x])))
@@ -108,3 +102,50 @@ print "Good URLS: ",urlList
 print "BAD LINKS: ",badLinks
 print "JPEGS: ", jpgAmount
 print "DOCIDS : ", documentIDs
+
+#FILE WRITING
+out = open('urlList.txt', 'w') #page List
+out.write('List of all urls visited (or at least attempted to):\n')
+for x in urlList:
+    out.write(x)
+    out.write("\n")
+out.close()
+
+out = open('outgoingLinks.txt', 'w') #outgoing links
+out.write('List of outgoing links:\n')
+for x in outgoingLinks:
+    out.write(x)
+    out.write("\n")
+out.close()
+
+out = open('badLinks.txt', 'w') #bad links
+out.write('List of bad links:\n')
+for x in badLinks:
+    out.write(x)
+    out.write("\n")
+out.close()
+
+
+
+out = open('jpegAmount.txt', 'w') #amount of jpegs
+out.write("JPEG AMOUNT: ")
+out.write(str(jpgAmount))
+out.close()
+
+out = open('wordIndex.txt', 'w') #amount of jpegs
+out.write("Word Index:\n")
+for x in words.keys():
+    out.write(x)
+    out.write(": ")
+    out.write(str(words[x]))
+    out.write('\n')
+out.close()
+
+out = open('frequentWords.txt', 'w')
+out.write("20 most frequent words and amount of occurences:\n")
+for x in frequentWords[:20]:
+    out.write(x[0])
+    out.write(": ")
+    out.write(str(x[1]))
+    out.write('\n')
+out.close()
